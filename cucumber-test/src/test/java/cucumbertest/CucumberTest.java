4ac9abc6a41e9fba9d1aba1b22e4abaa8e3c88f5
RAN <wangrandk@users.noreply.github.com>
2022-03-19 09:43:08 +0100

    create cucumber test

---
+++ cucumber-test/src/test/java/cucumbertest/CucumberTest.java
@@ -0,0 +1,12 @@
+package cucumbertest;
+
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "src/test/resources/featurefiles", publish = true)
+public class CucumberTest {
+
+}
---
+++ cucumber-test/src/test/resources/StepsDefinition.java
@@ -0,0 +1,116 @@
+
+public class StepsDefinition {
+	
+    //scenario move
+	@Given("your robot life is not {int}")
+	public void your_robot_life_is_not(Integer int1) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+	@Given("IsYourTurn status is true")
+	public void is_your_turn_status_is_true() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+	@Given("position x is {int} y is {int}")
+	public void position_x_is_y_is(Integer int1, Integer int2) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+	@When("move")
+	public void move() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+	@When("orientation is North")
+	public void orientation_is_north() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+	@Then("position x is {int} y is {int}")
+	public void position_x_is_y_is(Integer int1, Integer int2) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+	@When("orientation is West")
+	public void orientation_is_west() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+	@Then("position x is {int}  y is {int}")
+	public void position_x_is_y_is(Integer int1, Integer int2) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+	@When("orientation is East")
+	public void orientation_is_east() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+	@When("orientation is South")
+	public void orientation_is_south() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+
+	//scenario rorate
+
+	@Given("IsYourTurn status is true")
+	public void is_your_turn_status_is_true() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+	@When("orientation is North")
+	public void orientation_is_north() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+	@When("rotate to the left")
+	public void rotate_to_the_left() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+	@Then("orientation is West")
+	public void orientation_is_west() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+	@When("rotate to the right")
+	public void rotate_to_the_right() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+	@Then("orientation is East")
+	public void orientation_is_east() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+	@When("orientation is West")
+	public void orientation_is_west() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+	@Then("orientation is South")
+	public void orientation_is_south() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+	@Then("orientation is North")
+	public void orientation_is_north() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+	@When("orientation is South")
+	public void orientation_is_south() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+	@When("orientation is East")
+	public void orientation_is_east() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+
+}
