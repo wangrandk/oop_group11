6db8556c6e6054b33ea71d74e68043aef2fe9ae7
Giovanni Guerreiro <guerreiro.giovanni@gmail.com>
2022-04-15 13:53:39 +0200

    GameMaster Class

--- cucumber-test/src/main/java/model/GameMaster.java
+++ cucumber-test/src/main/java/model/GameMaster.java
@@ -12,27 +12,28 @@ public class GameMaster {
 	int countRounds = 0;
 	
 
-	
+	// Create Player a adds to the list of players
 	public void createPlayer() {
 		Player newPlayer = new Player(); // Create a new player
 		this.players.add(newPlayer); // add to the list
-		
 	}
 	
-	public ArrayList<Player> getPlayers() {
-		return this.players;
-	}
-	
-	public void startGame(ArrayList<Player> players) {
-		newRound(players);
-	
+	public void startGame(ArrayList<Player> players) {		
+		newRound(players, gameDeck);
 	}
 
+	// Constructor that should be called every new round.
 	public void newRound(ArrayList<Player> players) {
 		this.round = new Round(players);
 		countRounds++;
 	}
 	
+	
+	// Will be called just once during the creation of the game.
+	public void newRound(ArrayList<Player> players, Deck gameDeck) {
+		this.round = new Round(players);
+		countRounds++;
+	}
 	
 	public Round getRound() {
 		return round;
@@ -41,6 +42,11 @@ public class GameMaster {
 	public int getCountRounds() {
 		return countRounds;
 	}
+	
+	public ArrayList<Player> getPlayers() {
+		return this.players;
+	}
+	
 
 
 	
