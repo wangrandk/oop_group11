9df705a0fee164a2d2bbc0f7b7cacad19f40ee10
Giovanni Guerreiro <guerreiro.giovanni@gmail.com>
2022-04-21 11:40:12 +0200

    created controller package

--- cucumber-test/src/main/java/model/Game.java
+++ cucumber-test/src/main/java/model/Game.java
@@ -7,15 +7,6 @@ public class Game {
 
 	public static void main(String[] args) {
 		
-		Player player = new Player();
-		
-		Robot robot =  new Robot();
-		
-		player.setRobot(robot);
-
-		player.setLife(2);
-		
-		System.out.println(robot.getPlayer().getLife());
 		
 
 	}
