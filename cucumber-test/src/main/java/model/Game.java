d987269063a0efe38eb22e2c5a566382e8bb8143
Giovanni Guerreiro <guerreiro.giovanni@gmail.com>
2022-04-23 14:14:30 +0200

    heal feature implemented.

--- cucumber-test/src/main/java/model/Game.java
+++ cucumber-test/src/main/java/model/Game.java
@@ -1,14 +1,48 @@
 package model;
 
+import java.util.ArrayList;
+
+import model.board.Board;
+import model.board.EasyBoard;
+import model.card.Card;
+import model.card.Deck;
+import model.card.MoveOne;
+import model.card.SubDeck;
 import model.main.Player;
+import model.tile.Pit;
 import model.tile.Robot;
+import utilities.GameSettings;
+import utilities.Position;
 
 public class Game {
 
 	public static void main(String[] args) {
 		
+		// create the board
+		new EasyBoard();
 		
+		// create the game's deck
+		Deck deck = Deck.getInstance();
+	
+		// Create player 1 and 2
+		Player player1 = new Player();
+		Player player2 = new Player();
+		
+		// Set each player a Robot (we always know are the robot is our board)
+		player1.setRobot(Board.getTile(new Position(4,0)));
+		
+	
+		// While some of the players did not win, the game will Run
+		while (player.getPlayerStatus() != GameSettings.PlayerStatus.WON) {
+			// The game will be played in rounds.
+			
+			
+			
+			
+		}
 
+		
+		
 	}
 
 }
