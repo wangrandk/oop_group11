3be413f72083e6f4737009030146a7633ae46962
Md shariful alam <57239302+shajibdk@users.noreply.github.com>
2022-05-01 03:42:12 +0200

    refactoring

--- cucumber-test/src/main/java/controller/AppController.java
+++ cucumber-test/src/main/java/controller/AppController.java
@@ -4,8 +4,13 @@ import java.io.IOException;
 import view.GUI;
 import view.GameLaunch;
 
+// Application controller class
 public class AppController {
-	//AppController class will initialize whole application;
+	
+	
+	//AppController class will initialize whole application...
+	
+	
 	public static void main(String[] args) throws IOException {
 		new GameLaunch();
 	    new GUI();	
