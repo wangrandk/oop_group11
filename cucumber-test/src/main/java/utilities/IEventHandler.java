8310359f5f388feb47064eb6c20d2ccfb48f44e1
Giovanni Guerreiro <guerreiro.giovanni@gmail.com>
2022-05-01 20:28:26 +0200

    all tests are passing.

--- cucumber-test/src/main/java/utilities/IEventHandler.java
+++ cucumber-test/src/main/java/utilities/IEventHandler.java
@@ -5,10 +5,6 @@ public interface IEventHandler {
     /**
      * A listener class needs to implement this method.
      * Inside this is specified what to do when events that
-     * are of interest to the listener class is published.
-     * @param evt The event that is published.
-     * @param o The first object that follows along with the published event.
-     * @param o2 The second object that follows along with the published event.
      */
     void onEvent(EventList.Event evt, Object o1, Object o2);
 }
